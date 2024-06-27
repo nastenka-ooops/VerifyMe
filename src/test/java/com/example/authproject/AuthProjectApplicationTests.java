bea38bbcd79396348b8511b8a95cbd915f09d399
nastenka-ooops <nastenka.brutskaya@mail.ru>
2024-06-27 08:31:03 +0300

    init commit

---
+++ src/test/java/com/example/authproject/AuthProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
