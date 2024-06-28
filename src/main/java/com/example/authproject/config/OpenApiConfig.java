62dc99dd703192bc1a533ad7930ed13a07570db2
nastenka-ooops <nastenka.brutskaya@mail.ru>
2024-06-28 09:17:42 +0300

    update openApi policy

--- src/main/java/com/example/authproject/config/OpenApiConfig.java
+++ src/main/java/com/example/authproject/config/OpenApiConfig.java
@@ -2,6 +2,7 @@ package com.example.authproject.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,7 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .addServersItem(new Server().url("https://auth-project-production.up.railway.app"))
                 .info(new Info()
                         .title("Auth Project API")
                         .version("1.0")
